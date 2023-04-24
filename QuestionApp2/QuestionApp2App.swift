2c962ad399e7a3d45d846e9d2504da36d662f3ba
Tracey Bullington <traceylbullington@gmail.com>
2023-04-23 19:15:12 -0500

    Initial Commit

---
+++ QuestionApp2/QuestionApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionApp2App.swift
+//  QuestionApp2
+//
+//  Created by scholar on 4/23/23.
+//
+
+import SwiftUI
+
+@main
+struct QuestionApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
